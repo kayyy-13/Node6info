164545f992767cc5a95c7c6de037e1eca98507dc
kayyy-13 <tmj16live@gmail.com>
2025-03-06 16:24:42 -0300

    novo

---
+++ controllers/controller.js
@@ -0,0 +1,28 @@
+export async function home(req,res){
+    res.render('admin/index')
+}
+export async function abreaddtime(req, res) {
+    res.render('admin/time/add')
+}
+export async function addtime(req, res) {
+    res.redirect('/admin/time/add')
+}
+export async function listartime(req, res) {
+    res.render('admin/time/lst', '');
+}
+    export async function filtrartime(req, res) {
+        res.render('admin/time/lst', '');
+}
+export async function deletatime(req, res) {
+   res.redirect('/admin/time/lst')
+}
+export async function abreedttime(req, res){
+   
+    res.render('admin/time/edt','')
+}
+export async function edttime(req, res){
+    res.redirect('/admin/time/lst')
+}
+export async function addjogador(req, res){
+    res.render('/admin/time/jogador','')
+}
---
+++ routes/route.js
@@ -0,0 +1,33 @@
+import express from 'express';
+const router = express.Router();
+import {
+    home,
+    abreedttime,
+    edttime,
+    abreaddtime,
+    deletatime,
+    addtime,
+    listartime,
+    filtrartime,
+    addjogador,
+} from '../controllers/controller.js'
+router.get('/', home)
+//time
+//create do modelo time (create)
+router.get('/admin/time/add', abreaddtime)
+router.post('/admin/time/add', addtime)
+//rotas do modelo time (read)
+
+router.get('/admin/time/lst', listartime)
+router.post('/admin/time/lst', filtrartime)
+//rota do modelo time (delete)
+
+router.get('/admin/time/del/:id', deletatime)
+//rota do modelo time (editar)
+router.get('/admin/time/edt/:id', abreedttime)
+router.post('/admin/time/edt/:id', edttime)
+
+//rota para adicionar jogador
+router.get('/admin/time/jogador/:id', addjogador)
+router.post('/admin/time/jogador/:id', addjogador)
+export default router
